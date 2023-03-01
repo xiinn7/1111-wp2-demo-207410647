31674514d5f90e9a4535323ff8f6f697f7c7f7a4
xiinn7 <ruby19991017@gmail.com>
2023-03-01 21:38:27 +0800

    W03-P3: create card_xx tale in SUPABASE

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const db = require('./utils/database');
-const testDB = require('./utils/test-db')
+// const testDB = require('./utils/test-db')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,18 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+let pool;
+
+if(process.env.NODE_ENV === 'SUPABASE'){
+    pool = new Pool({
+    user: 'postgres',
+    host:  process.env.SUPABASE_HOST ,
+    port: '5432',
+    database: 'postgres',
+    password: 'process.env.SUPABASE_PASSWORD'
+    })
+    console.log(`connecting SUPABASE PostgreSQL database${pool.options.database}on port ${pool.options.port}`);
+
+}else{pool = new Pool({
 
     user: 'postgres',
     host: 'localhost',
@@ -11,5 +23,9 @@ const pool = new Pool({
 });
 
 console.log(`connecting local PostgreSQL database${pool.options.database}on port ${pool.options.port}`);
+
+};
+
+
 
 module.exports = pool;
